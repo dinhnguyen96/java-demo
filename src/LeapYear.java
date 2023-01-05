44a644f092cb3b15bcc3283dba04700fb3879c1d
dinhnguyen96 <39048935+dinhnguyen96@users.noreply.github.com>
2023-01-05 15:46:05 +0700

    update Leap Year exercise

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class LeapYear {
+import java.util.Scanner;
+
+public class LeapYear
+{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Nhập số năm : ");
+        int year = input.nextInt();
+
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+        {
+            System.out.println("Là năm nhuận");
+        }
+        else
+        {
+            System.out.println("Không phải Là năm nhuận");
+        }
+    }
 }
