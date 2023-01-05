146ffc0bc1d85014275f9ffc4c7299faffae7343
dinhnguyen96 <39048935+dinhnguyen96@users.noreply.github.com>
2023-01-05 11:18:29 +0700

    add Hello exercise

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Hello
+{
+    public static void main(String[] args)
+    {
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter your name : ");
+       String name = input.nextLine();
+
+       System.out.printf("Hello : %s", name);
+    }
+}
